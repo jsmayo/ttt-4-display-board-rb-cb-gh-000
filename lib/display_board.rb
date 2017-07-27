10fba38a5d3d8bf1f0dcf652078f8db667525736
Jesse Mayo <jsmayo91@gmail.com>
2017-07-27 07:52:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def printDivides
+  puts "   |   |   "
+end
+
+def printLines
+    puts "-----------"
+end
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} ";
+    printLines;
+    puts " #{board[3]} | #{board[4]} | #{board[5]} ";
+    printLines;
+    puts " #{board[6]} | #{board[7]} | #{board[8]} ";
+end
